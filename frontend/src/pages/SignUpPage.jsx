2dc56081d114685f0daa810f17067cad73179ea8
Madhu-Sudhana-Rao <154989674+Madhu-Sudhana-Rao@users.noreply.github.com>
2025-07-27 19:31:04 +0530

    Update SignUpPage.jsx

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { MessageCircle } from "lucide-react";
 import { Link } from "react-router-dom";
-import useSignup from "../hooks/useSignUp"; 
+import useSignup from "../hooks/useSignup";
 
 const SignUpPage = () => {
   const [signupData, setSignupData] = useState({
@@ -20,9 +20,7 @@ const SignUpPage = () => {
   return (
     <div className="w-screen h-screen overflow-hidden flex items-center justify-center" data-theme="dracula">
       <div className="flex w-full h-full max-w-6xl bg-base-100 shadow-lg rounded-lg overflow-hidden">
-        {/* Left Side - Form */}
         <div className="w-full lg:w-1/2 p-8 flex flex-col justify-center">
-          {/* App Logo and Name */}
           <div className="flex items-center gap-2 mb-6">
             <MessageCircle className="text-white w-6 h-6" />
             <span className="text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary">
@@ -30,14 +28,12 @@ const SignUpPage = () => {
             </span>
           </div>
 
-          {/* Error message */}
           {error && (
             <div className="alert alert-error text-sm mb-4 py-2">
               <span>{error.response?.data?.message || "Signup failed"}</span>
             </div>
           )}
 
-          {/* Signup Form */}
           <form onSubmit={handleSignup} className="space-y-4">
             <div>
               <h2 className="text-xl font-semibold">Create an Account</h2>
@@ -50,9 +46,9 @@ const SignUpPage = () => {
               type="text"
               placeholder="Your Name"
               className="input input-bordered w-full"
-              value={signupData.name}
+              value={signupData.fullName}
               onChange={(e) =>
-                setSignupData({ ...signupData, name: e.target.value })
+                setSignupData({ ...signupData, fullName: e.target.value })
               }
               required
             />
@@ -101,9 +97,7 @@ const SignUpPage = () => {
           </form>
         </div>
 
-        {/* Right Side - Image and Text */}
         <div className="hidden lg:flex flex-col w-1/2 h-full">
-          {/* Image */}
           <div className="flex-grow h-2/3">
             <img
               src="/image.jpeg"
@@ -111,7 +105,6 @@ const SignUpPage = () => {
               className="w-full h-full object-cover"
             />
           </div>
-          {/* Text */}
           <div className="bg-primary/10 p-4 text-center h-1/3 flex flex-col justify-center">
             <h2 className="text-lg font-semibold">
               Connect with language partners worldwide
